9fef02bf7b657cf95772a0fc2cec6f561dd76d10
Hamad Javed <hamadjaved69@gmail.com>
2021-03-27 00:35:02 +0500

    added input field and method to add todos

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import {
+  Button,
+  FormControl,
+  Input,
+  InputLabel
+} from '@material-ui/core';
 
 function App() {
+  const [todos, setTodos] = useState(['Take the dogs for a walk', 'Create React Project']);
+  const [input, setInput] = useState('');
+
+  const addTodo = (event) => {
+    console.log(input);
+    event.preventDefault();
+    setTodos([...todos, input]);
+    setInput('');
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello World</h1>
+      <form>
+        <FormControl>
+          <InputLabel>Todo</InputLabel>
+          <Input
+            value={input}
+            onChange={event => setInput(event.target.value)}
+            />
+        </FormControl>
+        <Button
+          variant="contained"
+          color="primary"
+          type="submit"
+          onClick={addTodo}
+          disabled={!input}
+          >
+          Primary
+        </Button>
+      </form>
+      <ul>
+        {
+          todos.map((todo, i) => (
+            <li key={i}>{todo}</li>
+          ))
+        }
+        <li></li>
+      </ul>
     </div>
   );
 }
