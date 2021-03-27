043d56666a2f97d6fb179096c254099c1a9e1fbf
Hamad Javed <hamadjaved69@gmail.com>
2021-03-28 04:48:28 +0500

    update and delete todo functionality

--- src/App.js
+++ src/App.js
@@ -2,25 +2,43 @@ import './App.css';
 import db from './firebase';
 import Todo from './Todo/Todo';
 import firebase from 'firebase';
+import Navbar from './Navbar/Navbar';
 import React, { useState, useEffect } from 'react';
 import {
-  Button,
-  FormControl,
+  Grid,
   Input,
-  InputLabel
+  Button,
+  InputLabel,
+  makeStyles,
+  FormControl
 } from '@material-ui/core';
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+  form: {
+    textAlign: "center"
+  }
+}));
+
 function App() {
+
+  const classes = useStyles();
   const [todos, setTodos] = useState([]);
   const [input, setInput] = useState('');
 
   // when the app loads we need to listen to the database and fetch the todos
   useEffect(() => {
-    //fires when app.js loads
-    db.collection('todos').orderBy('timestamp', 'desc').onSnapshot(snapshot => {
-      setTodos(snapshot.docs.map(doc => doc.data().todo));
-    })
-  }, []);
+    //this code fires when app.js loaded
+    db.collection('todos').orderBy("timestamp", "desc").onSnapshot(snapshot => {
+      setTodos(snapshot.docs.map(doc => ({ id:doc.id, todo: doc.data().todo})))
+    })}, [])
 
   const addTodo = (event) => {
     event.preventDefault();
@@ -33,36 +51,47 @@ function App() {
   }
 
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-      <form>
-        <FormControl>
-          <InputLabel>Todo</InputLabel>
-          <Input
-            value={input}
-            onChange={event => setInput(event.target.value)}
-            />
-        </FormControl>
-        <Button
-          variant="contained"
-          color="primary"
-          type="submit"
-          onClick={addTodo}
-          disabled={!input}
-          >
-          Add Todo
-        </Button>
-      </form>
-      <ul>
-        {
-          todos.map((todo, i) => (
-            <Todo
-              key={i}
-              text={todo}
-            />
-          ))
-        }
-      </ul>
+    <div className={classes.root}>
+      <Navbar />
+      <br />
+      <Grid container>
+        <Grid item xs={2} md={3}></Grid>
+        <Grid item xs={8} md={6}>
+          <form className={classes.form}>
+            <div>  
+              <FormControl>
+                <InputLabel>Todo</InputLabel>
+                <Input
+                  value={input}
+                  onChange={event => setInput(event.target.value)}
+                  />
+              </FormControl>
+            </div>
+            <div style={{padding:'10px'}}>
+              <Button
+                variant="contained"
+                color="primary"
+                type="submit"
+                onClick={addTodo}
+                disabled={!input}
+                >
+                Add Todo
+              </Button>
+            </div>
+          </form>
+          <div>
+            {
+              todos.map((todo, i) => (
+                <Todo
+                  key={i}
+                  todo={todo}
+                />
+              ))
+            }
+          </div>
+        </Grid>
+        <Grid item xs={2} md={3}></Grid>
+      </Grid>
     </div>
   );
 }
--- src/Todo/Todo.js
+++ src/Todo/Todo.js
@@ -1,18 +1,100 @@
 import './Todo.css';
-import React from 'react';
+import db from '../firebase';
+import React, { useState } from 'react';
+import EditIcon from '@material-ui/icons/Edit';
+import DeleteIcon from '@material-ui/icons/Delete';
 import {
     List,
+    Modal,
+    Input,
+    Button,
     ListItem,
-    ListItemText
+    makeStyles,
+    InputLabel,
+    IconButton,
+    FormControl,
+    ListItemText,
+    ListItemSecondaryAction,
 } from '@material-ui/core';
 
+function rand() {
+    return Math.round(Math.random() * 20) - 10;
+}
+
+function getModalStyle() {
+    const top = 50 + rand();
+    const left = 50 + rand();
+
+    return {
+        top: `${top}%`,
+        left: `${left}%`,
+        transform: `translate(-${top}%, -${left}%)`,
+    };
+}
+
+const useStyles = makeStyles((theme) => ({
+    paper: {
+        position: "absolute",
+        width: 400,
+        backgroundColor: theme.palette.background.paper,
+        border: "2px solid #000",
+        boxShadow: theme.shadows[5],
+        padding: "20px",
+        textAlign: "center",
+    }
+}));
+
 function Todo(props) {
+
+    const classes = useStyles();
+    const [open, setOpen] = useState(false);
+    const [modalStyle] = useState(getModalStyle);
+    const [input, setInput] = useState();
+
+    const updateTodo = () => {
+        db.collection('todos').doc(props.todo.id).set({
+            todo: input
+        }, { merge: true })
+        setOpen(false);
+    }
+
     return (
+        <>
+        <Modal
+            open={open}
+            onClose={e => setOpen(false)}
+        >
+            <div style={modalStyle} className={classes.paper}>
+                <h1>Update Todo</h1>
+                <FormControl>
+                <InputLabel>Todo</InputLabel>
+                <Input
+                    placeholder={props.todo.todo}
+                    value={input}
+                    onChange={e => setInput(e.target.value)}
+                    />
+                </FormControl>
+                <Button variant="contained" color="primary" type="submit"onClick={updateTodo}>Update Todo</Button>
+            </div>
+        </Modal>
         <List>
             <ListItem>
-                <ListItemText primary="Todo" secondary={props.text}></ListItemText>                
+                <ListItemText primary="Todo" secondary={props.todo.todo}></ListItemText>
+                <ListItemSecondaryAction>
+                    <IconButton edge="end" aria-label="edit" onClick={e => setOpen(true)}>
+                        <EditIcon />
+                    </IconButton>
+                    <IconButton
+                        edge="end"
+                        aria-label="delete"
+                        onClick={e => db.collection('todos').doc(props.todo.id).delete()}
+                        >
+                        <DeleteIcon />
+                    </IconButton>
+                </ListItemSecondaryAction>
             </ListItem>
         </List>
+        </>
     )
 }
 
